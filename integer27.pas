8557491679bfd9c9c9d4ef521de80d6ccf9d604c
Иван <johnny.kiv@yandex.ru>
2021-01-29 22:54:14 +0300

    n

---
+++ integer27.pas
@@ -0,0 +1,10 @@
+﻿
+program Integer27;
+var
+  K,D: Integer;
+begin
+  Write('Enter the day of the year: ');
+  Readln(K);
+  D:=((K+ 4) mod 7)+1;
+  Writeln('Day of the week : ',D);
+end.
---
+++ integer28.pas
@@ -0,0 +1,12 @@
+﻿program integer28;
+var 
+  a,b:integer;
+  c:real;
+begin
+  writeln('Enter the day of the year: ');
+  readln(a);
+  writeln('Enter the first day of the week of the year: ');
+  readln(b);
+  c:=((b+a-2)mod 7)+1;
+  write('Day of the week: ',c);
+end.
