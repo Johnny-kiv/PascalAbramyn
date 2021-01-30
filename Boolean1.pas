9a92bc0cd5788ca2faa3d1174051c422d51163ad
Иван <johnny.kiv@yandex.ru>
2021-01-30 18:01:48 +0300

    n

---
+++ Boolean1.pas
@@ -0,0 +1,10 @@
+﻿Program boolean1;
+var
+  b:integer;
+  c:boolean;
+begin
+  writeln('Enter the number');
+  readln(b);
+  c:=b>0;
+  write('This is a number ',c);
+end.
---
+++ case1.pas
@@ -0,0 +1,16 @@
+﻿program case1;
+var
+  a:integer;
+begin
+  writeln('Enter the number day of the week: ');
+  readln(a);
+  case a of
+    1:write('Monday');
+    2:write('Tuesday');
+    3:write('Wednesday');
+    4:write('Thursday');
+    5:write('Friday');
+    6:write('Saturday');
+    7:write('Sunday');
+  end;
+end.
