6ad2232033098964a54603e7f305b75a67bf1521
Иван <77494079+Johnny-kiv@users.noreply.github.com>
2021-05-05 13:06:31 +0300

    Create array1.pas

---
+++ array1.pas
@@ -0,0 +1,14 @@
+﻿program array1;
+var 
+ n,t :integer;
+ a:array [1..1000] of integer;
+begin
+  readln(n);  
+  t:=2;    
+  for var i:=1 to n do
+  begin
+    t:=t*i;
+    a[i]:=t;
+    write(a[i]+' ');
+  end;
+end.
