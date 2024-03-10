5895c61640cf6b54465d5aabed66c6b719c7f8a6
Иван <johnny.kiv@yandex.ru>
2024-03-10 16:39:58 +0300

    remake

--- array/array1.pas
+++ array/array1.pas
@@ -1,14 +1,13 @@
 ﻿program array1;
 var 
- n,t :integer;
- a:array [1..1000] of integer;
+  N,i :integer;
+  mas :array of integer
 begin
-  readln(n);  
-  t:=2;    
-  for var i:=1 to n do
+  write("Enter the number N") 
+  readln(N)
+  SetLenght
+  for i:=1 to N do
   begin
-    t:=t*i;
-    a[i]:=t;
-    write(a[i]+' ');
+    write(i);
   end;
 end.
--- begin/Begin2.pas
+++ begin/Begin2.pas
@@ -1,9 +1,9 @@
 ﻿program begin2;
 var a,s : integer;
 begin
-  write('Введите число ');
+  write('Enter the number: ');
   readln(a);
   s:=a*a;
-  write('Площадь квадрата: ',s);
+  write('The area of the square is ',s);
   
 end.
